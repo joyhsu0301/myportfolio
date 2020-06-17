2fd62b055aac601c6036efc851427edca5d9c555
joyhsu0301 <60234925+joyhsu0301@users.noreply.github.com>
2020-06-17 21:28:43 +0800

    added scrollbar and logo on click

--- scripts/main.js
+++ scripts/main.js
@@ -5,42 +5,56 @@ $(document).ready(function(){
 	var pngset = [
 
 		// {
-		// 	'name': 'This Website!',
-		// 	'image': 'png/scentlib.png',
+		// 	'name': 'Album Cover Re-design',
+		// 	'image': 'png/vowels.png',
 		// 	'height': '320',
 		// 	'width' : '500',
-		// 	'h_image': 'png/scentlib3.png',
+		// 	'h_image': 'png/vowels.png',
 		// 	'h_height': '320',
 		// 	'h_width': '500',
 		// 	'text': 'hello hello testing! testing, hope this is workingggg oh yay it is!'
 		// },
 
 		{
-			'name': 'Scent Library',
-			'image': 'png/scentlib.png',
-			'height': '320',
-			'width' : '500',
-			'h_image': 'png/scentlib3.png',
-			'h_height': '320',
-			'h_width': '500',
-			'text': 'hello hello testing! testing, hope this is workingggg oh yay it is!'
+			'name': 'This Website!',
+			'image': 'smiley.png',
+			'height': '200',
+			'width' : '220',
+			'h_image': 'png/thiswebsite.png',
+			'h_height': '620',
+			'h_width': '950',
+			'text': 'I have always wanted to create an online portfolio that best described me as a person and a designer. While I was creating this website, I made sure to do exactly that.'
 		},
 
 		{
-			'name': 'venmo',
-			'image': 'png/venmo.png',
-			'height': '200',
-			'width' : '200',
-			'h_image': 'png/venmo.gif',
-			'h_height': '200',
-			'h_width': '250'
+			'name': 'Scent Library',
+			'image': 'png/scentlib.png',
+			'height': '620',
+			'width' : '950',
+			'h_image': 'png/scentlib3.png',
+			'h_height': '620',
+			'h_width': '950',
+			'h2_image': 'png/scentlib2.png',
+			'h2_height': '620',
+			'h2_width': '950',
+			'text': 'For a final project, we were tasked with creating a website which somehow presented a collection of objects using JSON objects.<br><br>Mimicking an online retail experience, I decided to create a website containing a collection of scents familiar or interesting to me. I wanted to translate my experiences of and memories associated with these scents into an interactive webpage. <a href="https://joyhsu0301.github.io/core-lab/final/index.html" target="_blank" class="links">Go To The Website</a>'
 		},
+
+		// {
+		// 	'name': 'venmo',
+		// 	'image': 'png/venmo.png',
+		// 	'height': '200',
+		// 	'width' : '200',
+		// 	'h_image': 'png/venmo.gif',
+		// 	'h_height': '200',
+		// 	'h_width': '250'
+		// },
 
 		{
 			'name': 'Gold Is Boring',
 			'image': 'png/gib.png',
-			'height': '300',
-			'width' : '300',
+			'height': '500',
+			'width' : '500',
 			'h_image': 'png/gib2.png',
 			'h_height': '300',
 			'h_width': '400',
@@ -50,8 +64,8 @@ $(document).ready(function(){
 		{
 			'name': 'Bembo Type Poster',
 			'image': 'png/bembo.png',
-			'height': '300',
-			'width' : '200',
+			'height': '400',
+			'width' : '300',
 			'h_image': 'png/bembo2.png',
 			'h_height': '360',
 			'h_width': '200',
@@ -59,21 +73,10 @@ $(document).ready(function(){
 		}, 
 
 		{
-			'name': 'Friction Logo',
-			'image': 'png/frictionlogo.png',
-			'height': '200',
-			'width' : '350',
-			'h_image': 'png/frictionlogo.png',
-			'h_height': '200',
-			'h_width': '350',
-			'text': 'hello hello testing! testing, hope this is workingggg oh yay it is!'
-		}, 
-
-		{
 			'name': 'Vignelli Zine',
 			'image': 'png/massimo.png',
-			'height': '360',
-			'width' : '280',
+			'height': '560',
+			'width' : '430',
 			'h_image': 'png/massimo2.png',
 			'h_height': '300',
 			'h_width': '400',
@@ -81,10 +84,21 @@ $(document).ready(function(){
 		},
 
 		{
+			'name': 'Friction Logo',
+			'image': 'png/frictionlogo.png',
+			'height': '300',
+			'width' : '550',
+			'h_image': 'png/frictionlogo.png',
+			'h_height': '300',
+			'h_width': '550',
+			'text': 'hello hello testing! testing, hope this is workingggg oh yay it is!'
+		}, 
+
+		{
 			'name': 'Postcard',
 			'image': 'png/postcard.png',
-			'height': '240',
-			'width' : '360',
+			'height': '440',
+			'width' : '700',
 			'h_image': 'png/postcard2.png',
 			'h_height': '240',
 			'h_width': '360',
@@ -94,8 +108,8 @@ $(document).ready(function(){
 		{
 			'name': 'Dorm Room Illustrations',
 			'image': 'png/room.png',
-			'height': '320',
-			'width' : '400',
+			'height': '420',
+			'width' : '500',
 			'h_image': 'png/room2.png',
 			'h_height': '400',
 			'h_width': '300',
@@ -154,10 +168,11 @@ $(document).ready(function(){
 			$('#corner_text').fadeOut();
 			$('#goback').fadeIn();
 			$('body').css({'background-color': 'rgb(255,206,43)', 'transition': 'all 0.5s linear'});
+			// $('#png_box').css({'top': '10%'});
 			$('#png_box').children().not($(this)).remove();
 			$('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h_image+'" style="width: '+$(this).data("png").h_width+'px; height: '+$(this).data("png").h_height+'px;">');
 			$('#png_box').append('<div class="description">'+$(this).data("png").text+'</div>');
-			$('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h_image+'" style="width: '+$(this).data("png").h_width+'px; height: '+$(this).data("png").h_height+'px;">');
+			$('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h2_image+'" style="width: '+$(this).data("png").h2_width+'px; height: '+$(this).data("png").h2_height+'px;">');
 		});
 		$('#png_box').append(pngDiv);
 		}
@@ -169,6 +184,7 @@ $(document).ready(function(){
 	$('#goback').click(function(){
 		$('#png_box').empty();
 		originalpng(pngset);
+		$('#png_box').css({'top': '0px'});
 		// $('#expand_transition').css({'transform': 'scale(0.1)', 'background-color': 'none'});
 		$('#goback').fadeOut();
 		$('#corner_text').fadeIn();
@@ -200,7 +216,7 @@ $(document).ready(function(){
 	});
 
 	$('#random').click(function(){
-
+		$('.wall').toggle();
 	});
 
 

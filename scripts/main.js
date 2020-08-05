39d3b3ef8212cfb65df4c7e14014cc3ac65ead1b
joyhsu0301 <60234925+joyhsu0301@users.noreply.github.com>
2020-08-05 17:59:08 +0800

    took away on click of each project

--- scripts/main.js
+++ scripts/main.js
@@ -20,16 +20,7 @@ $(document).ready(function(){
 			'image': 'smiley.png',
 			'height': '200',
 			'width' : '220',
-			'h_image': 'png/thiswebsite.png',
-			'h_height': '600',
-			'h_width': '950',
-			'h2_image': 'png/thiswebsite3.png',
-			'h2_height': '600',
-			'h2_width': '950',
-			'h3_image': 'png/thiswebsite2.png',
-			'h3_height': '600',
-			'h3_width': '950',
-			'text': 'After creating the Scent Library project, I was inspired to create my own portfolio website with HTML, CSS and JavaScript. I wanted this website to best describe me as a person and a designer. <br><br> Be sure to keep browsing around this website!'
+			'link': 'online_portfolio.html'
 		},
 
 		{
@@ -37,16 +28,7 @@ $(document).ready(function(){
 			'image': 'png/scentlib.png',
 			'height': '600',
 			'width' : '950',
-			'h_image': 'png/scentlib3.png',
-			'h_height': '600',
-			'h_width': '950',
-			'h2_image': 'png/scentlib2.png',
-			'h2_height': '600',
-			'h2_width': '950',
-			'h3_image': 'png/scentlib4.png',
-			'h3_height': '600',
-			'h3_width': '950',
-			'text': 'For a final project, we were tasked with creating a website which somehow presented a collection of objects using JSON objects.<br><br>Mimicking an online retail experience, I decided to create a website containing a collection of scents familiar or interesting to me. I wanted to translate my experiences of and memories associated with these scents into an interactive webpage everyone can play around with. <a href="https://joyhsu0301.github.io/core-lab/final/index.html" target="_blank" class="links">Go To The Website</a>'
+			'link': 'online_portfolio.html'
 		},
 
 		// {
@@ -64,16 +46,7 @@ $(document).ready(function(){
 			'image': 'png/gib.png',
 			'height': '600',
 			'width' : '600',
-			'h_image': 'png/gib4.png',
-			'h_height': '470',
-			'h_width': '550',
-			'h2_image': 'png/gib2.png',
-			'h2_height': '470',
-			'h2_width': '650',
-			'h3_image': 'png/gib3.png',
-			'h3_height': '470',
-			'h3_width': '350',
-			'text': 'We were given an open-ended class assignment where we had the freedom to choose our own content and curate them into a designed booklet of some sort. <br><br>‘Gold is Boring’ is an anthology of articles written by various authors about their differing views on minimalism. The simple layout of the articles are contrasted with the Japanese snacks, a symbol of maximalism due to its mesmerizing packaging, chaotically placed over the spreads as the zine progresses. This reflects the conflicting opinions on minimalism as discussed in the articles throughout.'
+			'link': 'online_portfolio.html'
 		}, 
 
 		{
@@ -81,16 +54,7 @@ $(document).ready(function(){
 			'image': 'png/bembo.png',
 			'height': '400',
 			'width' : '300',
-			'h_image': 'png/bembo2.png',
-			'h_height': '600',
-			'h_width': '350',
-			'h2_image': 'png/bembo3.png',
-			'h2_height': '550',
-			'h2_width': '420',
-			'h3_image': 'png/bembo4.png',
-			'h3_height': '550',
-			'h3_width': '420',
-			'text': 'We were given an open-ended class assignment where we had the freedom to choose our own content and curate them into a designed booklet of some sort.'
+			'link': 'online_portfolio.html'
 		}, 
 
 		{
@@ -98,10 +62,7 @@ $(document).ready(function(){
 			'image': 'png/massimo.png',
 			'height': '650',
 			'width' : '500',
-			'h_image': 'png/massimo2.png',
-			'h_height': '300',
-			'h_width': '400',
-			'text': 'massimo vignelli yessiR!'
+			'link': 'online_portfolio.html'
 		},
 
 		{
@@ -109,10 +70,7 @@ $(document).ready(function(){
 			'image': 'png/frictionlogo.png',
 			'height': '350',
 			'width' : '650',
-			'h_image': 'png/frictionlogo.png',
-			'h_height': '300',
-			'h_width': '550',
-			'text': 'hello hello testing! testing, hope this is workingggg oh yay it is!'
+			'link': 'online_portfolio.html'
 		}, 
 
 		{
@@ -120,21 +78,15 @@ $(document).ready(function(){
 			'image': 'png/postcard.png',
 			'height': '540',
 			'width' : '830',
-			'h_image': 'png/postcard2.png',
-			'h_height': '240',
-			'h_width': '360',
-			'text': 'hello hello testing! testing, hope this is workingggg oh yay it is!'
+			'link': 'online_portfolio.html'
 		}, 
 
 		{
-			'name': 'Dorm Room Illustrations',
+			'name': 'Spaces',
 			'image': 'png/room.png',
 			'height': '520',
 			'width' : '600',
-			'h_image': 'png/room2.png',
-			'h_height': '400',
-			'h_width': '300',
-			'text': 'hello hello testing! testing, hope this is workingggg oh yay it is!'
+			'link': 'online_portfolio.html'
 		}
 
 	]
@@ -191,17 +143,18 @@ $(document).ready(function(){
 			$('#tail').empty();
 		})
 		.click(function(){
-			// $('#expand_transition').css({'transform': 'scale(110)', 'background-color': 'rgb(255,206,43)', 'z-index': '1'});
-			$('#corner_text').fadeOut();
-			$('#goback').fadeIn();
-			$('body').css({'background-color': 'rgb(255,206,43)', 'transition': 'all 0.5s linear'});
+			var url= $(this).data("png").link;
+			window.location.href= url;
+			// $('#corner_text').fadeOut();
+			// $('#goback').fadeIn();
+			// $('body').css({'background-color': 'rgb(255,206,43)', 'transition': 'all 0.5s linear'});
 			// $('#png_box').css({'top': '10%'});
-			$('#png_box').children().not($(this)).remove();
-			$('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h_image+'" style="width: '+$(this).data("png").h_width+'px; height: '+$(this).data("png").h_height+'px;">');
-			$('#png_box').append('<div class="description">'+$(this).data("png").text+'</div>');
-			$('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h2_image+'" style="width: '+$(this).data("png").h2_width+'px; height: '+$(this).data("png").h2_height+'px;">');
-			$('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h3_image+'" style="width: '+$(this).data("png").h3_width+'px; height: '+$(this).data("png").h3_height+'px;">');
-			$('#background').css({'display':'none'});
+			// $('#png_box').children().not($(this)).remove();
+			// $('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h_image+'" style="width: '+$(this).data("png").h_width+'px; height: '+$(this).data("png").h_height+'px;">');
+			// $('#png_box').append('<div class="description">'+$(this).data("png").text+'</div>');
+			// $('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h2_image+'" style="width: '+$(this).data("png").h2_width+'px; height: '+$(this).data("png").h2_height+'px;">');
+			// $('#png_box').append('<img class="ondisplay" src="'+$(this).data("png").h3_image+'" style="width: '+$(this).data("png").h3_width+'px; height: '+$(this).data("png").h3_height+'px;">');
+			// $('#background').css({'display':'none'});
 		});
 		$('#png_box').append(pngDiv);
 		}
